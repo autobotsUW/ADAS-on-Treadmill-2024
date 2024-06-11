1d541828b1530ea350bfacadaee2c17903074852
atlantis294 <115775673+atlantis294@users.noreply.github.com>
2024-06-11 09:39:16 -0400

    stanley controller

--- ros2_jf_waterloo/src/jf_package_waterloo/jf_package_waterloo/control_node.py
+++ ros2_jf_waterloo/src/jf_package_waterloo/jf_package_waterloo/control_node.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
-
+import math as m
+import numpy as np
 from std_msgs.msg import String,Int32MultiArray,Float32MultiArray
 
 
@@ -21,6 +22,10 @@ class Control(Node):
         self.last_error_angle=0
 
     def send_command(self):
+        """
+        Send the speed,angle to command topic
+
+        """
         center_servo=100
         delta_servo=20
         if self.speed>150:
@@ -43,6 +48,9 @@ class Control(Node):
         # self.get_logger().info("Send to car: {}".format(msg.data))
 
     def camera_sub_function(self, msg):
+        """
+        Read the car_position topic
+        """
         if len(msg.data)>0:
             self.Xcar=msg.data[0]
             self.Ycar=msg.data[1]
@@ -58,12 +66,18 @@ class Control(Node):
             self.error_sum_angle = 0
     
     def input_sub_function(self, msg):
+        """
+        Read the input topic
+        """
         self.Xinput=msg.data[0]
         self.Yinput=msg.data[1]
         # self.get_logger().info('Input position x: {}'.format(self.Xinput))
         # self.get_logger().info('Input position y: {}'.format(self.Yinput))
 
     def calculate_command(self):
+        """
+        Calculate speed and angle with the input and the position of the car
+        """
         current_time = self.get_clock().now()
         delta_time = (current_time - self.previous_time).nanoseconds / 1e9
         self.previous_time = current_time
@@ -83,23 +97,32 @@ class Control(Node):
         # Kd_angle = 0
 
         error_speed = self.Xinput - self.Xcar
-        error_angle = self.Yinput - self.Ycar
+        # error_angle = self.Yinput - self.Ycar
 
         if self.Xcar<300:
             self.error_sum_speed += error_speed * delta_time
-            self.error_sum_angle += error_angle * delta_time
+            # self.error_sum_angle += error_angle * delta_time
         
         
         # self.get_logger().info('Error speed: {:.3f}'.format(error_speed))
-        self.get_logger().info('Error angle: {:.3f}'.format(error_angle))
+        # self.get_logger().info('Error angle: {:.3f}'.format(error_angle))
         self.speed = int(Kp_speed * error_speed + Ki_speed * self.error_sum_speed + Kd_speed * (error_speed-self.last_error_speed)/delta_time)
-        self.angle = int(Kp_angle * error_angle + Ki_angle * self.error_sum_angle + Kd_angle * (error_angle-self.last_error_angle)/delta_time)
+        # self.angle = int(Kp_angle * error_angle + Ki_angle * self.error_sum_angle + Kd_angle * (error_angle-self.last_error_angle)/delta_time)
 
         self.last_error_speed=error_speed
-        self.last_error_angle=error_angle
+        # self.last_error_angle=error_angle
+
+
+        cross_track_error = self.Yinput - self.Ycar
+        k_stanley = 1.0 
+        # Stanley controller
+        heading_error = self.car_angle
+        stanley_control = heading_error + np.atan2(k_stanley * cross_track_error, self.speed)
+        self.angle = int(stanley_control * 180 / m.pi)
+
         
         
-        max_angle=min(10,abs(error_angle/5))
+        max_angle=10
         if (self.car_angle>max_angle and self.angle<0) or (self.car_angle<-max_angle and self.angle>0):
             self.angle=0     
         # self.get_logger().info('Command calculate: speed {} angle {}'.format(self.speed,self.angle)) 
