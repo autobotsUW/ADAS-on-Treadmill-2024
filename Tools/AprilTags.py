65a620b642fe21acce16c4ac7e6061ce1304b7ea
atlantis294 <115775673+atlantis294@users.noreply.github.com>
2024-06-13 15:13:20 -0400

    tags

--- Tools/AprilTags.py
+++ Tools/AprilTags.py
@@ -1,10 +1,15 @@
 import apriltag
 import cv2
+import time 
+start = time.time()
 image = cv2.imread("test.png")
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 options = apriltag.DetectorOptions(families="tag36h11")
 detector = apriltag.Detector(options)
 tags = detector.detect(gray)
 
+
 for tag in tags:
     print(tag.tag_id)   
+
+print("Processing time {:.3f}".format(time.time()-start))
